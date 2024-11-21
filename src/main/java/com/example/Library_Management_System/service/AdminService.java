96cecd051f6d6c0cff724d5afa9e1d87fdbcbc82
Mukta Deshpande <muktadeshpande000@gmail.com>
2024-11-21 16:37:42 +0530

    Added authorization logic to AdminService class

--- src/main/java/com/example/Library_Management_System/service/AdminService.java
+++ src/main/java/com/example/Library_Management_System/service/AdminService.java
@@ -1,6 +1,8 @@
 package com.example.Library_Management_System.service;
 
+import com.example.Library_Management_System.Utils.Constants;
 import com.example.Library_Management_System.model.Admin;
+import com.example.Library_Management_System.model.User;
 import com.example.Library_Management_System.repository.AdminDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,7 +12,19 @@ public class AdminService {
     @Autowired
     AdminDao adminDao;
 
-    public void createOrUpdateAdmin(Admin admin) {
+    @Autowired
+    UserService userService;
+
+    public void createOrUpdateAdmin(Admin admin) throws Exception {
+
+        User user = admin.getUser();
+        user = userService.save(Constants.ADMIN_USER, user);
+
+        if(user.getId() == null) {
+            throw new Exception("Invalid User");
+        }
+
+        admin.setUser(user);
         adminDao.save(admin);
     }
 
