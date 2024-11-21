ed3163d8077bfdf3c686de0599171a857775801e
Mukta Deshpande <muktadeshpande000@gmail.com>
2024-11-21 16:29:57 +0530

    Added username and password fields and modified the to() method

--- src/main/java/com/example/Library_Management_System/dto/CreateAdminRequest.java
+++ src/main/java/com/example/Library_Management_System/dto/CreateAdminRequest.java
@@ -1,6 +1,7 @@
 package com.example.Library_Management_System.dto;
 
 import com.example.Library_Management_System.model.Admin;
+import com.example.Library_Management_System.model.User;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,10 +21,20 @@ public class CreateAdminRequest {
     @NotBlank
     private String email;
 
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
     public Admin to() {
         return Admin.builder()
                 .name(this.name)
                 .email(this.email)
+                .user(User.builder()
+                        .username(this.username)
+                        .password(this.password)
+                        .build())
                 .build();
     }
 }
