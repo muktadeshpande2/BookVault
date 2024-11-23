41271fbd1d502f6bf9d696a8de79d2308f712b73
Mukta Deshpande <muktadeshpande000@gmail.com>
2024-11-23 12:50:40 +0530

    Modified post api and added a new get api

--- src/main/java/com/example/Library_Management_System/controller/StudentController.java
+++ src/main/java/com/example/Library_Management_System/controller/StudentController.java
@@ -2,12 +2,16 @@ package com.example.Library_Management_System.controller;
 
 import com.example.Library_Management_System.dto.*;
 import com.example.Library_Management_System.model.Student;
+import com.example.Library_Management_System.model.User;
 import com.example.Library_Management_System.service.StudentService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,11 +27,20 @@ public class StudentController {
 
     @PostMapping("/add")
     public ResponseEntity<String> addOrUpdateStudent(@RequestBody @Valid AddStudentRequest addStudentRequest) {
-        studentService.addStudentOrUpdate(addStudentRequest.to());
 
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body("Student Added Successfully");
+        try{
+            studentService.addStudentOrUpdate(addStudentRequest.to());
+
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body("Student Added Successfully");
+
+        } catch(Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(e.getMessage());
+        }
+
     }
 
 
@@ -44,6 +57,25 @@ public class StudentController {
 
     }
 
+    @GetMapping("/info")
+    public ResponseEntity<StudentResponse> getInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
 
+        Integer id = user.getStudent().getId();
 
+        try{
+            List<Student> studentList = studentService.searchStudent("id", String.valueOf(id));
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(studentList.getFirst().to());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(StudentResponse.createErrorResponse(e.getMessage()));
+        }
+    }
 }
