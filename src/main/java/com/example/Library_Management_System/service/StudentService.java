8702ed13f967f866b2c19cde278a834333d43a18
Mukta Deshpande <muktadeshpande000@gmail.com>
2024-11-23 12:36:39 +0530

    Modified addorupdatestudent and searchstudent methods to accommodate caching logic

--- src/main/java/com/example/Library_Management_System/service/StudentService.java
+++ src/main/java/com/example/Library_Management_System/service/StudentService.java
@@ -1,13 +1,14 @@
 package com.example.Library_Management_System.service;
 
+import com.example.Library_Management_System.Utils.Constants;
 import com.example.Library_Management_System.model.Student;
+import com.example.Library_Management_System.model.User;
+import com.example.Library_Management_System.repository.StudentCacheRepo;
 import com.example.Library_Management_System.repository.StudentDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -15,20 +16,46 @@ public class StudentService {
     @Autowired
     StudentDao studentDao;
 
-    public void addStudentOrUpdate(Student student) {
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    StudentCacheRepo studentCacheRepo;
+
+    public void addStudentOrUpdate(Student student) throws Exception {
+        User user = student.getUser();
+        user = userService.save(Constants.STUDENT_USER, user);
+
+        if(user.getId() == null) {
+            throw new Exception("Invalid User");
+        }
+
+        student.setUser(user);
         studentDao.save(student);
     }
 
     public List<Student> searchStudent(String searchKey, String searchValue) throws Exception {
         return switch (searchKey) {
-            case "id" -> {
-                Optional<Student> student = studentDao.findById(Integer.valueOf(searchValue));
-                yield student.map(List::of).orElseGet(ArrayList::new);
-            }
+            case "id" -> List.of(searchById(Integer.parseInt(searchValue)));
             case "rollNumber" -> studentDao.findByRollNumber(searchValue);
             case "name" -> studentDao.findByName(searchValue);
             case "email" -> studentDao.findByEmail(searchValue);
             default -> throw new Exception("Invalid Search Key: " + searchKey);
         };
+    }
+
+    public Student searchById(Integer studentId) {
+        //Checking if the details are present in the cache
+        Student student = studentCacheRepo.getStudent(studentId);
+
+        if(student != null) {
+            return student;
+        }
+
+        //If null i.e. not present in cache, then fetching from the DB or else creating new student
+         student = studentDao.findById(studentId).orElse(new Student());
+         studentCacheRepo.setStudent(student);
+
+        return student;
     }
 }
